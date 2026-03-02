ad70a45df56d2a9f9a03a6a417e32b7925ba3add
Seu Nome <theodiegocnsl@gmail.com>
2026-03-02 11:23:12 -0300

    02- Operadores Relacionais e Lógicos com JavaScript

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-let primeiroNumero = parseInt(Math.random()*10000);
-let segundoNumero = parseInt(Math.random()*500);
+let primeiroNumero = parseInt(Math.random()*10);
+let segundoNumero = parseInt(Math.random()*10);
 let resultado = primeiroNumero + segundoNumero;
+
 console.log(primeiroNumero, segundoNumero);
 console.log(resultado);
 
@@ -11,7 +12,44 @@ document.getElementById('elPrimeiroNumero').textContent = `Primeiro número sort
 document.getElementById('elSegundoNumero').textContent = `Segundo número sorteado = ${segundoNumero}`;
 
 
-
 document.getElementById('resultadoSoma').textContent = ` ${primeiroNumero} + ${segundoNumero} = ${resultado}`;
 
+resultado = primeiroNumero - segundoNumero
+document.getElementById('resultadoSubtracao').textContent = ` ${primeiroNumero} - ${segundoNumero} = ${resultado}`; 
 
+resultado = primeiroNumero * segundoNumero
+document.getElementById('resultadoMultiplicacao').textContent = ` ${primeiroNumero} * ${segundoNumero} = ${resultado}`; 
+
+resultado = primeiroNumero / segundoNumero
+document.getElementById('resultadoDivisao').textContent = ` ${primeiroNumero} / ${segundoNumero} = ${resultado.toFixed(2)}`; 
+
+resultado = primeiroNumero % segundoNumero
+document.getElementById('resultadoModulo').textContent = ` ${primeiroNumero} % ${segundoNumero} = ${resultado.toFixed(10)}`; 
+
+resultado = primeiroNumero === segundoNumero
+document.getElementById('igualdade').textContent = ` ${primeiroNumero} = ${segundoNumero}  ${resultado}`; 
+
+resultado = primeiroNumero !== segundoNumero
+document.getElementById('desigualdade').textContent = ` ${primeiroNumero} diferente de ${segundoNumero}  ${resultado}`; 
+
+resultado = primeiroNumero >= segundoNumero
+document.getElementById('maiorigual').textContent = ` ${primeiroNumero} >= ${segundoNumero}  ${resultado}`; 
+
+resultado = primeiroNumero <= segundoNumero
+document.getElementById('menorigual').textContent = ` ${primeiroNumero} <= ${segundoNumero}  ${resultado}`; 
+
+resultado = primeiroNumero < segundoNumero
+document.getElementById('menor').textContent = ` ${primeiroNumero} < ${segundoNumero}  ${resultado}`; 
+
+resultado = primeiroNumero > segundoNumero
+document.getElementById('maior').textContent = ` ${primeiroNumero} > ${segundoNumero}  ${resultado}`; 
+
+
+resultado = (primeiroNumero >= segundoNumero) || (segundoNumero > 3)
+document.getElementById('operadorOU').textContent = ` ${primeiroNumero} > ${segundoNumero} ou ${segundoNumero} > 3 = ${resultado}`; 
+
+resultado = (primeiroNumero >= segundoNumero) && (segundoNumero > 3)
+document.getElementById('operadorE').textContent = ` ${primeiroNumero} > ${segundoNumero} e ${segundoNumero} > 3 = ${resultado}`; 
+
+resultado = !((primeiroNumero >= segundoNumero) && (segundoNumero > 3))
+document.getElementById('operadorNAO').textContent = ` ${primeiroNumero} >= ${segundoNumero} e ${segundoNumero} > 3 = ${resultado}`; 
